5dbc2e3602f34f265f2ff589e98083981fa5131b
StevieCode <sdcowan@hotmail.com>
2018-11-05 22:59:14 +0000

    homework week 6 day1

---
+++ home_work.js
@@ -0,0 +1,259 @@
+//Episode 1
+
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Library',
+//   weapon: 'Rope'
+// };
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${scenario.murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+
+//"The murderer is miss Scarlet" // straight forward
+
+
+// Episode 2
+
+// const murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';            // can't be changeed
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+
+//"The murderer is Professor Plum" //the murderer can't be changed
+
+
+
+// Episode 3
+//
+// let murderer = 'Professor Plum';
+//
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+//
+// const secondVerdict = `The murderer is ${murderer}.`; //  as the firstVerdict can't be run it should say Second Verdict: Professor Plum
+// console.log('Second Verdict: ', secondVerdict);
+//
+// "First Verdict: Professor Plum"
+// "Second Verdict: Professor Plum"
+
+
+//Episode 4
+
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+//
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+//
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
+
+
+//"First log = The suspects are Miss Scarlet, Professor Plum, Colonel Mustard"
+//"Second log = Suspect three is Mrs Peacock"
+
+
+
+// Episode 5
+
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Kitchen',
+//   weapon: 'Candle Stick'
+// };
+//
+// const changeWeapon = function(newWeapon) {
+//   scenario.weapon = newWeapon;
+// }
+//
+// const declareWeapon = function() {
+//   return `The weapon is the ${scenario.weapon}.`;
+// }
+//
+// changeWeapon('Revolver');
+// const verdict = declareWeapon();
+// console.log(verdict);
+
+
+//"The weapon is the Revolver" // no funny business here, just followed it through
+
+
+
+
+// Episode 6
+
+// let murderer = 'Colonel Mustard';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+//
+//   const plotTwist = function() {
+//     murderer = 'Mrs. White';
+//   }
+//
+//   plotTwist();
+// }
+//
+// const declareMurderer = function () {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+
+//"The murderer is Mrs White"
+
+
+// Episode 7
+//
+// let murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+//
+//   const plotTwist = function() {
+//     let murderer = 'Colonel Mustard';
+//
+//     const unexpectedOutcome = function() {
+//       murderer = 'Miss Scarlet';
+//     }
+//
+//     unexpectedOutcome();
+//   }
+//
+//   plotTwist();
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+
+
+//"The murderer is Miss Scarlet" After looking i see its Mr Green. I get why
+
+
+// Episode 8
+
+// const scenario = {
+//   murderer: 'Mrs. Peacock',
+//   room: 'Conservatory',
+//   weapon: 'Lead Pipe'
+// };
+//
+// const changeScenario = function() {
+//   scenario.murderer = 'Mrs. Peacock';
+//   scenario.room = 'Dining Room';
+//
+//   const plotTwist = function(room) {
+//     if (scenario.room === room) {
+//       scenario.murderer = 'Colonel Mustard';
+//     }
+//
+//     const unexpectedOutcome = function(murderer) {
+//       if (scenario.murderer === murderer) {
+//         scenario.weapon = 'Candle Stick';
+//       }
+//     }
+//
+//     unexpectedOutcome('Colonel Mustard');
+//   }
+//
+//   plotTwist('Dining Room');
+// }
+//
+// const declareWeapon = function() {
+//   return `The weapon is ${scenario.weapon}.`
+// }
+//
+// changeScenario();
+// const verdict = declareWeapon();
+// console.log(verdict);
+
+
+
+//"The weapon is the lead pipe" Ok, its the candle stick, i get why
+
+
+
+
+
+// Episode 9
+//
+// let murderer = 'Professor Plum';
+//
+// if (murderer === 'Professor Plum') {
+//   let murderer = 'Mrs. Peacock';
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+//'The murderer is Professor Plum'
+
+
+
+
+
+
+
+// Episode 10
+
+// Did Steve really kill Upul with the Rubber Chicken, in the G9 Classroom????????????
+
+const scenario = {
+  murderer: 'Steve',
+  room: 'G9 Classroom',
+  weapon: 'Rubber Chicken',
+  victim: 'Upul'
+};
+
+const weaponChange = function(newWeapon) {
+      scenario.weapon = newWeapon }
+
+  if (scenario.weapon === "Rubber Chicken"){
+      scenario.victim = "Juan"
+      if (scenario.weapon !== "Bag Pipes")
+          scenario.room = "kitchen"
+      }
+
+weaponChange("Bag Pipes")
+
+//What really happened, run to find out!!
+
+console.log(`${scenario.murderer} killed ${scenario.victim}
+  with the ${scenario.weapon} in the ${scenario.room}`);
